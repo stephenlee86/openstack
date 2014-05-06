5ddc29816a23bafbafd26d20d73f1c28b12b9f38
Srinivasan <srinivasan.v.i.coep@gmail.com>
2014-05-05 22:34:26 -0400

    Added code color code for database security
    Added code color code for database security. Only valid for MySQL

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ This file has code that get information on the users, hosts and databases that a
 Can include other databases in future by providing wrapper apis  
 @author: srini
 '''
-import MySQLdb, json
+import MySQLdb, socket
 
 def getUserHostDBDict(ip,uname,pword):
     db = MySQLdb.connect(host=ip, user=uname,passwd=pword, db="mysql")
@@ -17,7 +17,32 @@ def getUserHostDBDict(ip,uname,pword):
         dict['host'] = row[1]
         dict['db'] = row[2]
         lidict.append(dict)
-    return json.dumps(lidict)
+    return lidict
 
+def getDBScan(ip,uname,pword):
+    results = getUserHostDBDict(ip, uname, pword)
+    names = []
+    names.append(ip)
+    names.append(socket.gethostbyname(ip))
+    ipv4_addr = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1] 
+    names.append(ipv4_addr)
+    names.append(socket.gethostbyaddr(ipv4_addr)[0])
+    names = list(set(names))
+    dictli = {}; s = ""
+    for result in results:
+        s = s + "," + str(result["host"])
+    li = s.strip(",").split(",")
+    for name in names:
+        li = [x for x in li if x != name]
+    dictli['hosts'] = li
+    if len(li)==0:
+        dictli["code"] = "Green"
+    elif len(li)<=3:
+        dictli["code"] = "Amber"
+    else:
+        dictli["code"] = "Red"
+    return dictli
+    
 if __name__ == '__main__':
-    getUserHostDBDict("localhost","root", "a")
+    print getUserHostDBDict("localhost","root", "a")
+    print getDBScan("localhost","root", "a")
